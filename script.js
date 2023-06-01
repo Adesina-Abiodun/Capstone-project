6ec4929ceb533e847d5aa4799b2954126e9dc576
Adesina-Abiodun <101020483+Adesina-Abiodun@users.noreply.github.com>
2023-06-01 16:19:18 +0100

    Capstone project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector('.menuIcon');
-const menu = document.querySelector('.secondNavlist');
-const menuOptions = document.querySelectorAll('.secondNavContent');
+const menu = document.querySelector('.main-nav-list');
+const menuOptions = document.querySelectorAll('.main-nav-item');
 const body = document.querySelector('body');
 
 function openMenu() {
@@ -44,73 +44,60 @@ const SpeakersData = [
   },
 ];
 
-function createSpeakerCard(speaker) {
-  return `
-    <li class="speaker speaker-${speaker.id}">
-        <div class="backFrame">
-            <img class="speakerImg" src="${speaker.speakerImage}" alt="speaker Picture">
-        </div>
-        <ul class="speaker-detail">
-            <li class="speaker-name">
-                <h3>${speaker.speakerName}</h3>
-            </li>
-            <li class="speaker-affiliation">
-                <h4>${speaker.speakerProfession}</h4>
-            </li>
-            <li class="speaker-description">
-                <p>${speaker.speakerInfo}</p>
-            </li>
-        </ul>
-    </li>
-  `;
-}
-
 function mySpeakers() {
   const featureSection = document.querySelector('.speakers');
   const overlay = document.createElement('div');
   overlay.classList.add('inner-div');
 
-  let cardContent = `
-    <h2>Featured Speakers</h2>
-    <hr class="hr">
-    <ul class="speakersList">
-  `;
+  let cardContent = `<h2>Featured Speakers</h2>
+        <hr class="hr">
+        <ul class="speakersList">`;
 
-  SpeakersData.forEach((speaker) => {
-    const speakerCard = createSpeakerCard(speaker);
-    cardContent += speakerCard;
-  });
+  for (let i = 0; i < SpeakersData.length; i += 1) {
+    const speakerInformation = SpeakersData[i];
 
-  cardContent += `
-    </ul>
+    cardContent += `
+                
+        <li class="speaker speaker-${i + 1}">
+            <div class="backFrame">
+                <img class="speakerImg" src="${speakerInformation.speakerImage}" alt="speaker Picture"></div>
+                <ul class="speaker-detail">
+                    <li class="speaker-name">
+                        <h3>${speakerInformation.speakerName}</h3>
+                    </li>
+                    <li class="speaker-affiliation">
+                        <h4>${speakerInformation.speakerProfession}</h4>
+                    </li>
+                    <li class="speaker-description">
+                        <p>${speakerInformation.speakerInfo}
+                        </p>
+                    </li>
+                </ul>
+        </li>`;
+  }
+
+  cardContent += `</ul>
     <button class="seeMoreBtn">More <i class="fa-solid fa-angle-down down-arrow"></i></button>
-    <button class="speaker-3 less">Less <i class="fa-solid fa-angle-up down-arrow"></i></button>
-  `;
-
+    <button class="speaker-3 less">Less <i class="fa-solid fa-angle-up down-arrow"></i></button>`;
   overlay.innerHTML = cardContent;
   featureSection.appendChild(overlay);
-
   const seeMoreBtn = document.querySelector('.seeMoreBtn');
   const seeLessBtn = document.querySelector('.less');
   const speaker3 = document.querySelector('.speaker-3');
   const speaker4 = document.querySelector('.speaker-4');
 
   function seeMore() {
-    seeMoreBtn.classList.remove('seeMoreBtn');
-    seeMoreBtn.classList.add('speaker-3');
+    seeMoreBtn.setAttribute('class', 'speaker-3');
     speaker3.classList.remove('speaker-3');
     speaker4.classList.remove('speaker-4');
-    seeLessBtn.classList.remove('speaker-3', 'less');
-    seeLessBtn.classList.add('seeMoreBtn');
+    seeLessBtn.setAttribute('class', 'seeMoreBtn');
   }
 
   function seeLess() {
-    seeLessBtn.classList.remove('seeMoreBtn');
-    seeLessBtn.classList.add('speaker-3', 'less');
+    seeLessBtn.setAttribute('class', 'speaker-3 less');
     speaker3.classList.add('speaker-3');
     speaker4.classList.add('speaker-4');
-    seeMoreBtn.classList.remove('speaker-3');
-    seeMoreBtn.classList.add('seeMoreBtn');
+    seeMoreBtn.setAttribute('class', 'seeMoreBtn');
   }
 
   seeMoreBtn.addEventListener('click', seeMore);
@@ -118,3 +105,4 @@ function mySpeakers() {
 }
 
 window.addEventListener('load', mySpeakers);
+
